961f26fd20b4e31e01c9f8e0d7ec7601512894d2
HAMarat <smile56680@gmail.com>
2023-01-31 01:44:25 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.json.ensure_ascii = False
+
+
+@app.route('/')
+def start_page():
+    """
+    Вьюшка для стартовой страницы
+    """
+    return 'Start_page'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
